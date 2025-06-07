973945c5357602bef322fe08a0588259e35441ce
Bella-514 <s1044013@gm.ncue.edu.tw>
2025-06-07 09:22:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,9 @@ from datetime import date
 
 st.set_page_config(layout="wide", page_title="這是Streamlit App第二次練習！")
 
-st.title("應用程式主頁")
+st.title("巴西森林大火2018-2020觀察")
 
-st.markdown(
-    """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io), [GEE](https://earthengine.google.com/), 
-    [geemap](https://leafmap.org) and [leafmap](https://leafmap.org). 
-    """
-)
-
-st.header("Instructions")
-
-markdown = """
-1. You can use it as a template for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python file.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
-"""
-
-st.markdown(markdown)
-
-
-
-
-st.title("選擇日期區間")
-
+st.header("選擇日期區間")
 
 # 初始化 session_state
 #if 'start_date' not in st.session_state:
